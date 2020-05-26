97a0694bd82d411bb6db85c85dd17f73d7a4749a
shwld <shu.account@outlook.com>
2020-05-26 05:44:48 +0000

    Add graphql-autotest spec

---
+++ spec/graphql/auto_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "GraphQL::Autotest" do
+  it do
+    expect {
+      runner = GraphQL::Autotest::Runner.new(
+        schema: AppSchema,
+        context: { },
+        logger: Logger.new(STDOUT)
+      )
+      runner.report!
+    }.not_to raise_error
+  end
+end
