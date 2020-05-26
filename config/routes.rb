7decbab8d1131d987e58b8da50883cb9a35c4543
yubachiri <k.i.moso9532@gmail.com>
2020-05-26 23:35:53 +0900

    Introduce LetterOpenerWeb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   if Rails.env.development?
     post '/schema', to: 'graphql_schema#execute'
     get '/graphiql', to: 'graphiql#show'
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
