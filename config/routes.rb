f8d869f880b955ce263915463eb4bb2f85c0f6bd
shwld <shu.account@outlook.com>
2020-05-29 14:14:05 +0900

    Tweak devise templates

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   root to: 'view#show'
 
-  get '*path' => 'view#show', constraints: lambda { |req|
-    req.path.exclude? 'rails/active_storage'
-  }
-
   if Rails.env.development?
     post '/schema', to: 'graphql_schema#execute'
     get '/graphiql', to: 'graphiql#show'
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
+  get '*path' => 'view#show', constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
 end
