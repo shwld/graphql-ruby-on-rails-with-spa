107c4e927a2a37d102ffab10d86e660a4568cec7
shwld <shu.account@outlook.com>
2020-06-03 15:35:07 +0900

    Add generators initialize file

---
+++ config/initializers/generators.rb
@@ -0,0 +1,15 @@
+Rails.application.config.generators do |g|
+  g.orm :active_record, primary_key_type: :uuid
+  g.javascripts false
+  g.stylesheets false
+  g.helper false
+  g.test_framework :rspec,
+    fixtures: true,
+    view_specs: false,
+    helper_specs: false,
+    request_specs: false,
+    routing_specs: false,
+    controller_specs: false,
+    model_specs: false
+  g.fixture_replacement :factory_bot, dir: "spec/factories"
+end
