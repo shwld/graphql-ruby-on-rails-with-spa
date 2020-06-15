1f388fb2a22ac6fffdfa123b5eeb9cc8c9532353
shwld <shu.account@outlook.com>
2020-06-15 15:24:35 +0900

    Add graphql_channel

---
+++ app/channels/graphql_channel.rb
@@ -0,0 +1,57 @@
+class GraphqlChannel < ApplicationCable::Channel
+  def subscribed
+    @subscription_ids = []
+  end
+
+  def execute(data)
+    query = data["query"]
+    variables = ensure_hash(data["variables"])
+    operation_name = data["operationName"]
+    context = {
+      channel: self,
+    }
+
+    result = AppSchema.execute({
+      query: query,
+      context: context,
+      variables: variables,
+      operation_name: operation_name
+    })
+
+    payload = {
+      result: result.to_h,
+      more: result.subscription?,
+    }
+
+    if result.context[:subscription_id]
+      @subscription_ids << result.context[:subscription_id]
+    end
+
+    transmit(payload)
+  end
+
+  def unsubscribed
+    @subscription_ids.each { |sid|
+      AppSchema.subscriptions.delete_subscription(sid)
+    }
+  end
+
+  private
+
+    def ensure_hash(ambiguous_param)
+      case ambiguous_param
+      when String
+        if ambiguous_param.present?
+          ensure_hash(JSON.parse(ambiguous_param))
+        else
+          {}
+        end
+      when Hash, ActionController::Parameters
+        ambiguous_param
+      when nil
+        {}
+      else
+        raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+      end
+    end
+end
