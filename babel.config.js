6e424a715e7c5346ef3d0cee7cc6b4d81358e3fe
shwld <shu.account@outlook.com>
2020-07-16 14:21:50 +0900

    Update around js compiling

--- babel.config.js
+++ babel.config.js
@@ -35,7 +35,9 @@ module.exports = function (api) {
           exclude: ['transform-typeof-symbol']
         }
       ],
-      ['babel-preset-typescript-vue', { allExtensions: true, isTSX: true }]
+      // ['@babel/preset-typescript', { allExtensions: true, isTSX: true }],
+      ['babel-preset-typescript-vue', { allExtensions: true, isTSX: true }],
+      ['@babel/preset-typescript', { 'allExtensions': true, 'isTSX': true }]
     ].filter(Boolean),
     plugins: [
       'babel-plugin-macros',
