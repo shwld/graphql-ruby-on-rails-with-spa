838088919fa04f53e50da0a87ab88b021d0d107c
shwld <shu.account@outlook.com>
2020-07-17 13:58:34 +0900

    Fix error

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -1,6 +1,6 @@
 import '@/styles/application.sass'
 import Rails from '@rails/ujs'
-import ActiveStorage from '@rails/activestorage'
+import * as ActiveStorage from '@rails/activestorage'
 
 Rails.start()
 ActiveStorage.start()
