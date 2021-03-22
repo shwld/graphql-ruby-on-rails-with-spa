d5c76978ab2b89a5940f4bc434c4cc795b9e99f9
shwld <shu.account@outlook.com>
2021-03-22 09:43:33 +0900

    date_time type

--- app/graphql/types/objects/user_type.rb
+++ app/graphql/types/objects/user_type.rb
@@ -34,4 +34,6 @@ class Types::Objects::UserType < Types::BaseObject
 
   field :id, ID, null: false
   field :email, String, null: true
+
+  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
 end
