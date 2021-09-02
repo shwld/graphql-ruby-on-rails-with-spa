5a8e62ad3a170e1c5a190e5b3cd27d33dd4b98a3
shwld <shu.account@outlook.com>
2021-09-03 08:00:24 +0900

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -11,12 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   // https://github.com/zeit/next.js/issues/8663#issue-490553899
-  globals: {
-    // we must specify a custom tsconfig for tests because we need the typescript transform
-    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
-    // can see this setting in tsconfig.jest.json -> "jsx": "react"
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json',
-    },
-  },
+  globals: {},
 }
