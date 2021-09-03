bddf4fcc2c89c4cfbbf54999a3fe6430455db883
shwld <shu.account@outlook.com>
2021-09-03 09:14:57 +0900

    Fix

--- app/javascript/src/pages/Chat.tsx
+++ app/javascript/src/pages/Chat.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
+import { ChatForm } from '../components/organisms/ChatForm'
 import { CommentList } from '../components/organisms/CommentList'
 
 export function Chat() {
   return (
     <>
       <div className="flex flex-col justify-center items-center pt-10">
-        <Chat />
+        <ChatForm />
         <CommentList className="mt-5" />
       </div>
     </>
--- app/javascript/src/pages/Index.tsx
+++ app/javascript/src/pages/Index.tsx
@@ -4,8 +4,13 @@ import { Link } from 'react-router-dom'
 import { useCurrentUser } from '@/src/hooks/currentUser'
 
 export function Index() {
-  const { currentUser } = useCurrentUser()
-  if (!currentUser) return <></>
+  const { currentUser, loading } = useCurrentUser()
+  if (!loading && currentUser == null)
+    return (
+      <>
+        <a href="/users/sign_in">Sign in</a>
+      </>
+    )
   return (
     <>
       <Link to="/chat">Go to chat</Link>
